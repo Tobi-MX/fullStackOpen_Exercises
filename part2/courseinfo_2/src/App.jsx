c0c3341ce926adc0aede900d2063709cf7107e5e
Tobi-MX <tobimustapha801@gmail.com>
2024-11-10 14:01:31 +0100

    Submitting Exercise_2.5

--- part2/courseinfo_2/src/App.jsx
+++ part2/courseinfo_2/src/App.jsx
@@ -1,42 +1,4 @@
-const Part = ({ partName, exercises }) => {
-  return(
-    <p>{partName} {exercises}</p>
-  )
-}
-
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((s, part) => s + part.exercises, 0)
-
-  return(
-    <p><strong>total of {total} exercises</strong></p>
-  )
-}
-
-
-const Content = ({ parts }) => {
-  return(
-    <div>
-      {parts.map((part) => {
-       return <Part key={part.id} partName={part.name} exercises={part.exercises} /> 
-      })}
-    </div>
-  )
-}
-
-
-const Header = ({ courseName }) => <h2>{courseName}</h2>
-
-
-const Course = ({ course }) => {
-  return(
-    <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 
 const App = () => {
