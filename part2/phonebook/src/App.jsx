af2ac529be3eab1abce64ce91bfbc9ede7a7856b
Tobi-MX <tobimustapha801@gmail.com>
2024-11-14 01:27:53 +0100

    Submitting Exercise_2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const SinglePerson = ({ person }) =>{
   return(
@@ -42,16 +43,21 @@ const Filter = ({ text, value, onWrite}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [showPersons, setShowPersons] = useState(persons)
   const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3002/persons')
+    .then(response => {
+      console.log('fetched successfully from server')
+      setPersons(response.data)
+      setShowPersons(response.data)
+    })
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
