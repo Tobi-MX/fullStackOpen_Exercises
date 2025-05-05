84feaa6c55493818e5cf56653f03ef2ad87e1e73
Tobi <Tobimustapha801@gmail.com>
2025-05-05 01:35:52 +0100

    Starting data for countries, and submitting Exercise_2.18

---
+++ part2/dataForCountries/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const App = () => {
+  const [allCountries, setAllCountries] = useState([])
+  const [searchValue, setSearchVale] = useState('')
+  const [filterDisplay, setFilterDisplay] = useState([])
+  const [chosen, setChosen] = useState('')
+  const [countryDisplay, setCountryDisplay] = useState(null)
+
+  useEffect(() => {
+    axios
+    .get(`https://studies.cs.helsinki.fi/restcountries/api/all/`)
+    .then(response => {
+      const allCountriesName = response.data.map(country => country.name.common)
+      setAllCountries(allCountriesName)
+    })
+  }, [])
+  
+  useEffect(() => {
+    if (chosen) {
+      axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${chosen}`)
+      .then(response => {
+        const countryData = response.data
+        setCountryDisplay(
+          <>
+            <h1>{countryData.name.common}</h1>
+            <div>Capital {countryData.capital}</div>
+            <div>Area {countryData.area}</div>
+
+            <h2>Languages</h2>
+            <ul>
+            {Object.values(countryData.languages).map(lan => <li key={lan}>{lan}</li>)}
+            </ul>
+
+            <img src={countryData.flags.png} alt={countryData.flags.src} />
+            
+            
+          </>
+        )
+      })
+    }
+    
+  }, [chosen])
+
+  const onSearch = (event) => {
+    const search = event.target.value
+    setSearchVale(search)
+
+    const filteredCountry = allCountries.filter(country => 
+      country.toLowerCase().includes(search.toLowerCase()) && search !== '')
+
+    if (filteredCountry.length === 1) {
+      setChosen(filteredCountry[0])
+    }else{
+      setChosen(null)
+      console.log('H')
+    }
+    setFilterDisplay(filteredCountry)
+  }
+
+  return(
+    <div>
+      <div> find countries <input value={searchValue} onChange={onSearch} /> </div>
+      
+      {filterDisplay.length > 10 ? 
+      <div>Too many searches, specify another filter</div>
+      : filterDisplay.length === 1 ? 
+      countryDisplay  
+      :filterDisplay.map(country => <div key={country}>{country}</div>)
+      }
+    </div>
+    
+  )
+}
+
+export default App
